bdb7bccd99b50f978dc825bd7b2332a47a4ff967
suzuki.sh <s2terminal@users.noreply.github.com>
2018-12-12 12:19:51 +0900

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source .env
+source $(pwd)/.env
 
 curl -s https://ip-ranges.amazonaws.com/ip-ranges.json | jq -r '.prefixes[].ip_prefix' | awk '{print "deny",$1";","#From AWS"}'
 aws ec2 describe-addresses | jq -r '.Addresses[] | [.PublicIp, .InstanceId] | @tsv' | awk '{print "allow",$1"/32;","# Instance-ID:"$2}'
